dd8615c92eac45b70df46342f23882171f19b2a7
Shubhi Mishra <shubhi2006m@gmail.com>
2026-07-15 18:36:44 +0530

    Time: 15 ms (30.24%), Space: 27.4 MB (98.16%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.cpp
+++ 0221-maximal-square/0221-maximal-square.cpp
@@ -1,21 +1,22 @@
 class Solution {
 public:
-    int maximalSquare(vector<vector<char>>& mat) {
-        int mx=0, r=mat.size(), c=mat[0].size();
-        vector<vector<int>>dp(r+1, vector<int>(c+1,0));
+    int maximalSquare(vector<vector<char>>& matrix) {
+        int r=matrix.size(), c=matrix[0].size(), mx=0;
+        vector<int>curr(c+1,0);
+        vector<int>next(c+1,0);
         for(int i=r-1; i>=0; i--){
             for(int j=c-1; j>=0; j--){
-                int right=dp[i][j+1];
-                int down=dp[i+1][j];
-                int diagonal=dp[i+1][j+1];
-                if(mat[i][j]=='1'){
-                    dp[i][j]=1+min({right,down,diagonal});
-                    mx=max(dp[i][j],mx);
-                }
-                else{
-                    dp[i][j]=0;
+                int right=curr[j+1];
+                int down=next[j];
+                int diagonal=next[j+1];
+                if(matrix[i][j]=='1'){
+                    curr[j]=1+min({right, down, diagonal});
+                    mx=max(mx,curr[j]);
+                }else{
+                    curr[j]=0;
                 }
             }
+            next=curr;
         }
         return mx*mx;
     }
