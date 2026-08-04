851ac306a780216402940ce28d6ed7bda3adeecb
Shubhi Mishra <shubhi2006m@gmail.com>
2026-08-04 18:35:46 +0530

    Time: 0 ms (100%), Space: 8.2 MB (5.93%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
@@ -10,7 +10,15 @@ public:
         return dp[n]=ans;
     }
     int numTrees(int n) {
-        vector<int>dp(n+1,-1);
-        return solve(n,dp);
+        vector<int>dp(n+1,0);
+        // return solve(n,dp);
+
+        dp[0]=dp[1]=1;
+        for(int i=2; i<=n; i++){
+            for(int j=1; j<=i; j++){
+                dp[i]+=dp[j-1]*dp[i-j];
+            }
+        }
+        return dp[n];
     }
 };
